96611618d8ce7b13151d426b7127f3fdd5e4d1ad
Anmol Ratan TIwari <66309398+artiwari10@users.noreply.github.com>
2025-03-23 20:05:53 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 require('dotenv').config();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://camusdevmatch-frontend.vercel.app",
   credentials: true,
 }));
 app.use(express.json());
